cf231fa4308f0fedfa0f675c650a75c455944072
vasili8 <tal7a.tmam@gmail.com>
2020-05-16 18:36:49 +0300

    Updates

---
+++ precache-manifest.65981a9d649a5732fc1143c5033c45fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a46d81c18231bc950909c0d17460491",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "4fb53d2ed3fc4cd4bbbf",
+    "url": "/coronatracker/static/css/main.4b59ecb6.chunk.css"
+  },
+  {
+    "revision": "707e03dd983f15506ef0",
+    "url": "/coronatracker/static/js/2.0712aeea.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/coronatracker/static/js/2.0712aeea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb53d2ed3fc4cd4bbbf",
+    "url": "/coronatracker/static/js/main.8c35a570.chunk.js"
+  },
+  {
+    "revision": "10da214f576ab0b06afc",
+    "url": "/coronatracker/static/js/runtime-main.a331f488.js"
+  }
+]);
